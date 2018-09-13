9180a7c23802e7f969bf8968b2ea75c40eada5ea
Alexander Schwartzberg <aeksco@gmail.com>
2018-09-13 00:57:32 -0400

    Added FormInput component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="form-group">
+    <label>
+      {{ label }}
+      <span class='text-danger' v-if="required">*</span>
+    </label>
+    <small class="form-text text-muted" v-if="example && help">{{example}}<br>{{help}}</small>
+    <small class="form-text text-muted" v-else-if="help">{{help}}</small>
+
+    <input v-if="type === 'BOOL'" type="checkbox" ref="input_el" class="form-control" :checked="value" @change="updateModel()">
+    <input v-else-if="type === 'DATE'" type="date" ref="input_el" class="form-control" :placeholder="placeholder" :value="value" @input="updateModel()" >
+    <input v-else-if="type === 'TIME'" type="time" ref="input_el" class="form-control" :placeholder="placeholder" :value="value" @input="updateModel()" >
+    <input v-else type="text" ref="input_el" class="form-control" :placeholder="placeholder" :value="value" @input="updateModel()" >
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['required', 'label', 'type', 'example', 'ex', 'help', 'placeholder', 'value'],
+  methods: {
+    updateModel () {
+      if (this.type === 'BOOL') {
+        this.$emit('input', this.$refs.input_el.checked)
+      } else {
+        this.$emit('input', this.$refs.input_el.value)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.form-group {
+  label {
+    margin-bottom: 0;
+  }
+  small {
+    margin-bottom: .6rem;
+  }
+}
+</style>
